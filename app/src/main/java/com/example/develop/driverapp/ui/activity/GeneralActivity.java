d1abd526f4b07eeac0dcfa4cc17055d143f93a14
teeeses <teeeses@mail.ru>
2016-06-06 09:27:58 +0300

    3 fragments

--- app/src/main/java/com/example/develop/driverapp/ui/activity/GeneralActivity.java
+++ app/src/main/java/com/example/develop/driverapp/ui/activity/GeneralActivity.java
@@ -1,20 +1,14 @@
 package com.example.develop.driverapp.ui.activity;
 
-import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.KeyEvent;
-import android.view.inputmethod.EditorInfo;
-import android.widget.TextView;
+import android.support.v4.app.FragmentActivity;
 
 import com.example.develop.driverapp.R;
-import com.github.pinball83.maskededittext.MaskedEditText;
 
 /**
  * Created by develop on 02.06.2016.
  */
-public class GeneralActivity extends Activity {
-
+public class GeneralActivity extends FragmentActivity {
 
 
     @Override
